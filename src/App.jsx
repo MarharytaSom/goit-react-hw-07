2c61cafade89e029a632de0daf4e24926f402f58
MarharytaSom <158225902+MarharytaSom@users.noreply.github.com>
2025-04-16 17:06:30 +0200

    1
    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,39 @@
 import { useDispatch, useSelector } from "react-redux";
-import { fetchContacts } from "./redux/contactsOps";
-import { selectFilteredContacts } from "./redux/contactsSlice";
-import ContactForm from "./components/ContactForm/ContactForm";
-import ContactList from "./components/ContactList/ContactList";
-import Filter from "./components/Filter/Filter";
-import styles from "./App.module.css";
+import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
+import SearchBox from "./SearchBox/SearchBox";
+import Loader from "./Loader/Loader";
 
-const App = () => {
+import {
+  selectError,
+  selectLoading,
+} from "../redux/contacts/contacts.selectors";
+import { useEffect } from "react";
+import { fetchContacts } from "../redux/contactsOps";
+
+function App() {
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
   const dispatch = useDispatch();
-  const contacts = useSelector(selectFilteredContacts);
 
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
-
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Книга контактів</h1>
+    <div>
+      <h1>Phonebook</h1>
       <ContactForm />
-      <Filter />
-      <ContactList contacts={contacts} />
+
+      <SearchBox />
+      <div>
+        {loading && !error && <Loader />}
+        {error !== null && (
+          <p style={{ color: "red", textAlign: "center" }}>{error}</p>
+        )}
+      </div>
+      <ContactList />
     </div>
   );
-};
+}
 
 export default App;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,20 +1,25 @@
-import { useDispatch } from "react-redux";
-import { deleteContact } from "../../redux/contactsOps";
-import styles from "./ContactList.module.css";
+import { useSelector } from "react-redux";
+import Contact from "../Contact/Contact.jsx";
+import css from "./ContactList.module.css";
+import { selectFilteredContacts } from "../../redux/contacts/contacts.selectors.js";
 
-const ContactList = ({ contacts }) => {
-  const dispatch = useDispatch();
+function ContactList() {
+  const contacts = useSelector(selectFilteredContacts);
 
   return (
-    <ul className={styles.list}>
-      {contacts.map(({ id, name, phone }) => (
-        <li key={id}>
-          {name}: {phone}
-          <button onClick={() => dispatch(deleteContact(id))}>Видалити</button>
-        </li>
-      ))}
-    </ul>
+    (<p>ContactList </p>),
+    (
+      <ul className={css.contactList}>
+        {contacts.map((item) => (
+          <Contact
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            number={item.number}
+          />
+        ))}
+      </ul>
+    )
   );
-};
-
+}
 export default ContactList;
